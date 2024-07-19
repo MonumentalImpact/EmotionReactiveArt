874eb5c61abb5be7f76e218d7a39eeb2fd094322
Kent Hutson <kenthutson@gmail.com>
2024-07-18 19:47:16 -0600

    testusb

--- test/test_usb_cam.py
+++ test/test_usb_cam.py
@@ -6,17 +6,18 @@ testpic = 'pics/testusbimage.jpg'
 cam = cv2.VideoCapture(0)
 
 while True:
-	ret, image = cam.read()
-	cv2.imshow('Imagetest - press any key to save',image)
-	k = cv2.waitKey(1)
-	if k != -1:
-		break
+    ret, image = cam.read()
+    cv2.imshow('Imagetest - press any key to save',image)
+    k = cv2.waitKey(1)
+    if k != -1:
+        break
 cv2.imwrite(testpic, image)
 cam.release()
 cv2.destroyAllWindows()
 
 objs = DeepFace.analyze(img_path = testpic, 
-    actions = ['emotion']
+    actions = ['age', 'gender', 'race', 'emotion'],
+    enforce_detection=False
 )
 print(f"analyze {testpic}:\n",objs)
 print("dominant emotion is ", objs[0]['dominant_emotion'])
