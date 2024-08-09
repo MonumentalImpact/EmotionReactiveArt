1dddf1a182e2d71486c344c3358ab975f0c8f5e1
Kent Hutson <kenthutson@gmail.com>
2024-08-09 14:40:54 -0600

    readface

--- demo/readface_button_labels_pub.py
+++ demo/readface_button_labels_pub.py
@@ -1,23 +1,23 @@
 import numpy as np
 import cv2
 from deepface import DeepFace
-import os
+# import os
 import json
 
 # button=gp21
 # led=gp20
 
-from time import sleep
+# from time import sleep
 import gpiozero
-from signal import pause
+# from signal import pause
 from sys import exit
 
 from paho.mqtt.client import Client 
 client =Client()
 
-if client.connect("pi5ub.local", 1883, 60) != 0:
+if client.connect("pi5ub.local", 1883, keepalive=3600) != 0:
     print("could not connect")
-    sys.exit(1)
+    exit(1)
 else:
     print("connected to broker")
 
@@ -29,20 +29,12 @@ button = gpiozero.Button(21)
 button.when_pressed = led.on
 button.when_released = led.off
 
-"""
-emotions:
-angry, disgust, fear, happy, sad, surprise, neutral
 
-put folders containing the images to be show in the 'pics' folder
-each folder under 'pics' should have 7 images, one for each emotion named as
-    <emotion>.jpg
-"""
-pics =  'pics'
-dirs = os.listdir(pics)
-print(f"rotating through the follow folders : {dirs}")
-cur_dir = dirs[0]
-num_dirs = len(dirs)
+emotions = ["angry", "happy", "sad", "fear","neutral","disgust", "surprise"]
+#angry, disgust, fear, happy, sad, surprise, neutral
 
+
+lastpic_file = 'lastpic.jpg'
 testpic = 'testusbimage.jpg'
 
 cam = cv2.VideoCapture(0)
@@ -55,16 +47,21 @@ emo_count = 7 #change cur_dir after emo_count changes in emotion
 
 print("starting main loop")
 q=ord('q') #key to quit
-win_title = 'Imagetest - press big red button to read emotion'
+win_title = 'Emotion Reactive Art - press big red button to read emotion'
+cv2.namedWindow(win_title,cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty(win_title,cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+
 while True:
     while True:
         ret, cam_image = cam.read()
-        emo_image = cv2.imread('pics/' + cur_dir + '/' + emotion+'.jpg')
+        emo_image = cv2.imread(lastpic_file)
         image = np.concatenate((cam_image,emo_image), axis=1)    
+
+        cv2.namedWindow(win_title,cv2.WND_PROP_FULLSCREEN)
+        cv2.setWindowProperty(win_title,cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
         cv2.imshow(win_title,image)
-        
         cv2.displayOverlay(win_title, status_msg)
-#         cv2.imshow('Imagetest - press big red button to read emotion',cam_image)
+
         if button.is_pressed:
             break
         k = cv2.waitKey(1)
@@ -77,6 +74,7 @@ while True:
     cv2.imwrite(testpic, cam_image)
     try:
         print('processing images....')
+        cv2.displayOverlay(win_title, 'processing images....')
         objs = DeepFace.analyze(img_path = testpic, 
             actions = ['emotion']
         )
@@ -85,8 +83,7 @@ while True:
         emotiondetail = json.dumps(sorted(objs[0]['emotion'].items(),key=lambda k: (k[1],k[0]), reverse=True))
         print("current dominant emotion is ", emotion)
         print(f"emotion details: {emotiondetail}")
-        emo_file = 'pics/' + cur_dir + '/' + emotion + '.jpg'
-        cv2.imwrite( emo_file, cam_image)
+        cv2.imwrite( lastpic_file, cam_image)
         analyzed = True
         status_msg = emotion + " : " + emotiondetail
 
@@ -94,7 +91,7 @@ while True:
     except:
         print('analysis failed')
         analyzed = False
-        status_msg = f"Analysis failed last time.  Current emotion is {emotion}"
+        status_msg = f"Analysis failed last time.  Current emotion is {emotion}. Possible emotions are {' '.join(emotions)}."
         pass
     
 #     if analyzed and emotion != last_emotion:
